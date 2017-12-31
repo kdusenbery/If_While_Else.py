13215e9a334a448d2a755f0897ef71a1959d0fa1
Kohl R Dusenbery <31451970+kdusenbery@users.noreply.github.com>
2017-12-31 04:44:48 -0600

    Add files via upload

---
+++ If_While_Else.py
@@ -0,0 +1,19 @@
+"""
+Kohl Dusenbery
+ENTD200
+Prof. Edeki
+Week 5
+"""
+
+a=1
+b=a+1
+c=a+b
+if c<5:
+    print ('c is false, the loop with repeat until condition is met.')
+count=0
+while(count<c):
+    print ('loop number:', count)
+    count=count+1
+else:
+    print ('c is greater than or equal to 5, the condition has been met!')
+
